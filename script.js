08821412df3f80affcc074aa3227580f9a8118c7
zeno <zeno.bcit@gmail.com>
2024-05-17 21:11:47 -0700

    Update 1.0.5 by Zeno

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 const cards = document.querySelectorAll(".card");
 
 let cardOne, cardTwo;
+let disableTracker = false;
+  
+function soundEffectPlay(sound){
+  var audio = new Audio(`sounds/${sound}.mp3`);
+  audio.play();
+}
 
 function flipCard(e) {
-    let clickedCard = e.target;
-    if(clickedCard !== cardOne) {
-        clickedCard.classList.add("flip");
-        if(!cardOne) {
-            return cardOne = clickedCard;
-        }
-        cardTwo = clickedCard;
-        
-        let cardOneImg = cardOne.querySelector("img"),
-        cardTwoImg = cardTwo.querySelector("img");
-        matchCards(cardOneImg, cardTwoImg);
+  soundEffectPlay('flip');
+  let clickedCard = e.target;
+  if (clickedCard !== cardOne && !disableTracker) {
+    clickedCard.classList.add("flip");
+    if (!cardOne) {
+        return cardOne = clickedCard;
     }
+    cardTwo = clickedCard;
+    disableTracker = true;
+    let cardOneImg = cardOne.querySelector(".cardImage").src,
+    cardTwoImg = cardTwo.querySelector(".cardImage").src;
+    matchCards(cardOneImg, cardTwoImg); 
+  }
 }
 
 function matchCards(img1, img2) {
-    console.log(img1, img2);
+  if (img1 === img2) {
+    soundEffectPlay("perfect");
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = "";
+    return disableTracker = false;
+  }
+
+  soundEffectPlay("wrong");
+  setTimeout(() => {
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
+
+  setTimeout(() => {
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = "";
+    disableTracker = false;
+  }, 1200);
 }
 
 cards.forEach(card => {
